ae6906d50019794ce30b03134db1a2c87cb23466
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-08-17 13:36:26 +0300

    Use comprehensions in templates

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -2,24 +2,33 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
+  @templates_path Path.expand("templates", File.cwd!)
+
+  # defp bear_item(bear) do
+  #   "<li>#{bear.name} - #{bear.type}</li>"
+  # end
+
+  # \\ [] sets an empty list as the default
+  defp render(template, conv, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+    %{conv | status: 200, resp_body: content}
   end
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join("\n")
-    # Transform bears to an HTML list. <ul><li>Name</li></ul>
-    %{conv | status: 200, resp_body: "<ul>\n#{items}\n</ul>"}
+
+    render("index.eex", conv, [bears: bears])
   end
 
   def show_bear(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "Bear #{bear.id}: #{bear.name}"}
+
+    render("show.eex", conv, [bear: bear])
   end
 
   def create(conv, %{"type" => type, "name" => name} = _params) do
